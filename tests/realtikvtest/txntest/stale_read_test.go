3a536830b457e6a5b4f75e32f416e768d961c05d
flaky-claw <flakyclaw@pingcap.com>
2026-07-06 19:16:20 +0000

    fix: stabilize flaky issue #69666

--- tests/realtikvtest/txntest/stale_read_test.go
+++ tests/realtikvtest/txntest/stale_read_test.go
@@ -124,15 +124,22 @@ func TestExactStalenessTransaction(t *testing.T) {
 		t.Log(testcase.name)
 		require.NoError(t, failpoint.Enable("tikvclient/injectTxnScope", fmt.Sprintf(`return("%v")`, testcase.zone)))
 		tk.MustExec(testcase.preSQL)
+		var freshTSLowerBound uint64
+		if !testcase.IsStaleness {
+			var err error
+			freshTSLowerBound, err = store.GetOracle().GetTimestamp(context.Background(), &oracle.Option{})
+			require.NoError(t, err)
+		}
 		tk.MustExec(testcase.sql)
 		require.Equal(t, testcase.IsStaleness, tk.Session().GetSessionVars().TxnCtx.IsStaleness)
 		if testcase.expectPhysicalTS > 0 {
 			require.Equal(t, testcase.expectPhysicalTS, oracle.ExtractPhysical(tk.Session().GetSessionVars().TxnCtx.StartTS))
 		} else if !testcase.IsStaleness {
-			curTS := oracle.ExtractPhysical(oracle.GoTimeToTS(time.Now()))
-			startTS := oracle.ExtractPhysical(tk.Session().GetSessionVars().TxnCtx.StartTS)
-			require.Less(t, curTS-startTS, time.Second.Milliseconds())
-			require.Less(t, startTS-curTS, time.Second.Milliseconds())
+			freshTSUpperBound, err := store.GetOracle().GetTimestamp(context.Background(), &oracle.Option{})
+			require.NoError(t, err)
+			startTS := tk.Session().GetSessionVars().TxnCtx.StartTS
+			require.GreaterOrEqual(t, startTS, freshTSLowerBound)
+			require.LessOrEqual(t, startTS, freshTSUpperBound)
 		}
 		tk.MustExec("commit")
 	}
